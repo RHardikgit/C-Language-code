bd3d1416e984d2abba78f7e6d3cbf4983c13a31f
Rathva Hardik <rtwhardik719@gmail.com>
2026-09-10 09:11:58 +0530

    Accending / Decanding order program

---
+++ decanding_order.c.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10];
+    printf(" -- enter numbers ::  ");
+
+    for(int i=0; i<10; i++){
+        scanf("%d",&arr[i]);
+    }
+   
+    int max;
+    for (int i = 0; i <10;i++){
+        for (int j = i +1; j <10; j++){
+        if (arr[i] < arr[j]){
+            max =  arr[i];
+            arr[i] = arr[j];
+            arr[j] = max;
+        } 
+        }
+    }
+
+    printf("\n-- Arrays in decending  order --\n");
+    for (int i=0; i< 10; i++){
+        printf(" %d",arr[i]);
+    }
+     return 0;
+}
